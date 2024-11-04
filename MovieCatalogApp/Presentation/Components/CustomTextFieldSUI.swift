43ca27e3946b9d65b113a8b520f2daca122c9ce8
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-11-05 04:03:13 +0700

    Text editor paddings fixed

--- MovieCatalogApp/Presentation/Components/CustomTextFieldSUI.swift
+++ MovieCatalogApp/Presentation/Components/CustomTextFieldSUI.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 struct CustomTextFieldView: View {
     @Binding var text: String
-        var placeholder: String
-
-        var body: some View {
-            ZStack(alignment: .topLeading) {
-                if text.isEmpty {
-                    Text(placeholder)
-                        .foregroundColor(.grayFaded)
-                }
-                TextEditor(text: $text)
-                    .foregroundColor(.textDefault)
-                    .lineLimit(0)
-                    .scrollContentBackground(.hidden)
-                    .background(Color.clear)
-                    .padding(.vertical, -8)
-                    .padding(.horizontal, -6)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    var placeholder: String
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(.grayFaded)
             }
-            .padding(16)
-            .frame(maxHeight: 120)
-            .background(Color.darkFaded)
-            .cornerRadius(8)
+            TextEditor(text: $text)
+                .foregroundColor(.textDefault)
+                .lineLimit(0)
+                .scrollContentBackground(.hidden)
+                .background(Color.clear)
+                .padding(.vertical, -8)
+                .padding(.horizontal, -6)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         }
+        .padding(16)
+        .frame(maxHeight: 120)
+        .background(Color.darkFaded)
+        .cornerRadius(8)
+    }
 }
